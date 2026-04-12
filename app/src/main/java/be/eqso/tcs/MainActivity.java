583ab6fb9958fbce613cd524f66f430a1de60889
TheKenny7321 <118374794+TheKenny7321@users.noreply.github.com>
2026-04-12 14:07:36 +0200

    6.0.2

--- app/src/main/java/be/eqso/tcs/MainActivity.java
+++ app/src/main/java/be/eqso/tcs/MainActivity.java
@@ -859,6 +859,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @JavascriptInterface
-        public String getAppVersion() { return "6.0.1"; }
+        public String getAppVersion() { return "6.0.2"; }
     }
 }
